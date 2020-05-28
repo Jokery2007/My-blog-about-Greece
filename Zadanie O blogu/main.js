9c382d39e2147e3b449fbd2e94a4efb1a4bf61c8
Jokery2007 <61429464+Jokery2007@users.noreply.github.com>
2020-05-28 13:55:05 +0200

    Add files via upload

---
+++ Zadanie O blogu/main.js
@@ -0,0 +1,5 @@
+alert('Chyba fajny pomysł z pisaniem stronki tylko na jedno zadanie :)')
+let takczynie = prompt ('Poproszę o odpowiedz na temat czy taki format jest ok')
+console.log = takczynie;
+
+
